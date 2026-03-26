2e776716cae24a345bd9c25e7dcefdfefef0cd95
itscharlesm <itscharlesm@gmail.com>
2026-03-26 13:54:06 +0800

    reset layout locations and polishing

--- resources/views/layouts/themes/main.blade.php
+++ resources/views/layouts/themes/main.blade.php
@@ -1,25 +1,28 @@
-@include('themes.partials.auth')
+@include('layouts.partials.auth')
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        @include('themes.partials.head')
-    </head>
-    <body class="hold-transition sidebar-mini layout-fixed">
-        <div class="wrapper">
-            @include('sweetalert::alert')
-            {{-- @include('admin.themes.partials.preloader') --}}
-            @include('themes.partials.navbar')
-            @include('themes.partials.sidebar')
-            <div class="content-wrapper">
-                @yield('content')
-                <a class="btn btn-primary back-to-top no-print" id="back-to-top" role="button" aria-label="Scroll to top" href="#">
-                    <i class="fas fa-chevron-up"></i>
-                </a>
-            </div>
-            @include('themes.partials.footer')
-            @include('themes.partials.controlsidebar')
-            @include('themes.partials.modals')
+
+<head>
+    @include('layouts.partials.head')
+</head>
+
+<body class="hold-transition sidebar-mini layout-fixed">
+    <div class="wrapper">
+        @include('sweetalert::alert')
+        {{-- @include('layouts.partials.preloader') --}}
+        @include('layouts.partials.navbar')
+        @include('layouts.partials.sidebar')
+        <div class="content-wrapper">
+            @yield('content')
+            <a class="btn btn-primary back-to-top no-print" id="back-to-top" role="button" aria-label="Scroll to top"
+                href="#">
+                <i class="fas fa-chevron-up"></i>
+            </a>
         </div>
-        @include('themes.partials.scripts')
-    </body>
+        @include('layouts.partials.footer')
+        @include('layouts.partials.controlsidebar')
+    </div>
+    @include('layouts.partials.scripts')
+</body>
+
 </html>
